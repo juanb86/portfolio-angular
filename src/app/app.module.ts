15950b47a06def9d974c655d127d8d3756c064ef
juanb86 <juan.e.begnis@gmail.com>
2023-02-24 14:38:15 -0300

    Creo componentes encabezado, hero y acercade. Creo servicio de datos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { HeroComponent } from './componentes/hero/hero.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    HeroComponent,
+    AcercaDeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/servicios/portfolio.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PortfolioService {
+  constructor(private http: HttpClient) {}
+
+  obtenerDatos(): Observable<any> {
+    console.log('LLAMADA DESDE SERVICIO');
+    return this.http.get('./assets/data/data.json');
+  }
+}
